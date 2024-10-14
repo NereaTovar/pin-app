a2a6f27241192c477a38247c6b8eb1576daf7ba1
Nerea <163418422+NereaTovar@users.noreply.github.com>
2024-10-14 12:45:14 +0200

    Vercel BE

--- bff/src/index.ts
+++ bff/src/index.ts
@@ -17,36 +17,22 @@
 // export const config = getConfig();
 
 
-import { config as dotenvConfig } from 'dotenv';
-import express from 'express';
-import { Config, ProcessVariables } from './config/config.type';
-import { getCommonConfig } from './config/common';
 
-// Configurar dotenv para las variables de entorno
+import express from 'express';
+import { config as dotenvConfig } from 'dotenv';
+
+// Carga las variables de entorno
 dotenvConfig();
 
-function getConfig(): Config {
-  return getCommonConfig(process.env as unknown as ProcessVariables);
-}
-
-export const config = getConfig();
-
-export function isLiveEnvironment(config: Config) {
-  return (
-    config.environment === 'staging' || config.environment === 'production'
-  );
-}
-
-// Crear una instancia de Express
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Configurar una ruta simple para probar el backend
+// Configurar una ruta básica
 app.get('/api', (req, res) => {
-  res.send('API is working!');
+  res.send('API funcionando!');
 });
 
 // Iniciar el servidor
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Servidor corriendo en el puerto ${port}`);
 });
